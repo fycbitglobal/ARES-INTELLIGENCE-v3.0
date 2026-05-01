03e3c246050170bcbce08b0684710f744c43c485
Fycbit <83513785+fycbitglobal@users.noreply.github.com>
2026-05-01 21:27:00 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 @dataclass
 class Config:
-    BOT_TOKEN: str = os.getenv("8647623541:AAEXP5hSkJb1FtmRLdN8g8RxL3iYKDI4vV0", "")
+    BOT_TOKEN: str = os.getenv("yourbotTOKEN", "")
     CRYPTO_PANIC_API: str = os.getenv("CRYPTO_PANIC_API", "")
     OLLAMA_URL: str = os.getenv("OLLAMA_URL", "http://127.0.0.1:11434/api/chat")
     MODEL: str = os.getenv("MODEL", "qwen2.5")
